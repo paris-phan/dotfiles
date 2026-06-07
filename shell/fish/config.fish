66e88c8855438f1bc8e57feb8e79280ffbab4132
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2026-06-07 14:00:38 -0500

    orbstack

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -38,3 +38,7 @@ if [ -f '/Users/parisphan/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users
 
 # Added by Antigravity CLI installer
 set -gx PATH "/Users/parisphan/.local/bin" $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
