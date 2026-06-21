31b36f081d6ed80ae5d055848655293154b9ce1f
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2026-06-21 14:19:17 -0500

    config

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -42,3 +42,6 @@ set -gx PATH "/Users/parisphan/.local/bin" $PATH
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by Antigravity IDE
+fish_add_path /Users/parisphan/.antigravity-ide/antigravity-ide/bin
