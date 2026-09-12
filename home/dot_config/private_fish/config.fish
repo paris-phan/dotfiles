f08aec24cbcf78fbd0872041b1bb006543ad64d8
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2026-09-12 17:21:29 -0400

    fish config fix

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,12 +1,15 @@
+fish_add_path -g $HOME/.local/bin
+
+test -f $HOME/.orbstack/shell/init2.fish
+and source $HOME/.orbstack/shell/init2.fish
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    type -q starship; and starship init fish | source
+
+    set -g fish_greeting "hi paris :)"
+
+    # Modern ls
+    alias ls='eza --icons --group-directories-first'
+    alias ll='eza --icons --long --header --git'
+    alias la='eza --icons --long --header --git --all'
 end
-
-starship init fish | source
-
-set -g fish_greeting "hi paris :)"
-
-# Modern ls
-alias ls='eza --icons --group-directories-first'
-alias ll='eza --icons --long --header --git'
-alias la='eza --icons --long --header --git --all'
